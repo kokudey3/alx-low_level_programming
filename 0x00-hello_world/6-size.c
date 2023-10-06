8c08445ac97229d8f4f0db1e281c95d6bd8c6e5f
kokudey3 <deyqwaku@gmail.com>
2023-10-05 19:26:05 -0700

    Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 /**
- * main - the main function
+ * main - Entry point
  *
- * Return:  0 when successful
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -13,6 +13,5 @@ printf("Size of int: %lu byte(s)\n", sizeof(int));
 printf("Size of long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of float: %lu byte(s)\n", sizeof(float));
-
 return (0);
 }
