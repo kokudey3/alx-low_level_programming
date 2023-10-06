d7d14ab06a99ec2b998df911c74b5d893f0b0ad3
kokudey3 <deyqwaku@gmail.com>
2023-10-05 17:42:50 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-    puts("\"Programming is building like a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
