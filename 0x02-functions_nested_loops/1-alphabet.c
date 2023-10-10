68fb12ba41052d3391a138331e35ad9fe92bddbd
Kokudey3 <deyqwaku@gmail.com>
2023-10-10 13:17:58 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase to the stdout
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int alpha = 97;
+	int max = 97 + 26;
+
+	while (alpha < max)
+	{
+		_putchar(alpha++);
+	}
+	_putchar('\n');
+}
