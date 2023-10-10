c216daa8f35e56deb3a49eda0996ded3a92f9b15
Kokudey3 <deyqwaku@gmail.com>
2023-10-10 13:32:50 -0700

    Nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
-/*
- * print_alphabet_x10 - print 10 times the alphabet, in lowercase to stdout
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase to stdout
  *
  * Return: void
  */
-
 void print_alphabet_x10(void)
 {
 	int i = 0;
 	int alpha;
 	int max;
 
-	while (i<10)
+	while (i < 10)
 	{
 		alpha = 97;
 		max = alpha + 26;
