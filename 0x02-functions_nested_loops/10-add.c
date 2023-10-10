e2d147be0dc718c58715adfe56cd78c9487ed47e
Kokudey3 <deyqwaku@gmail.com>
2023-10-10 15:29:26 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - prints adds two intergers and returns the result
+ *@n1: print int n1
+ *@n2: print int n2
+ * Return: Always 0
+ */
+
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
