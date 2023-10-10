b6a9518c821add7a91be17a71a49475a905420d5
Kokudey3 <deyqwaku@gmail.com>
2023-10-10 14:44:48 -0700

    Nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - Function prints the sign of a number
- * @n: number to print the sign 
+ * @n: number to print the sign
  *
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
