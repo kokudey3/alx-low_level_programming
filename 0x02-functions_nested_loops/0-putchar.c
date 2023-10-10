7fd810d7b461047d9c5a6a610ec3fa74383fedb7
Kokudey3 <deyqwaku@gmail.com>
2023-10-10 13:10:35 -0700

    Nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
 /**
- * Betty documentation 
+ * main - Prints '_putchar' to stdout
+ *
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
- char *str = "_putchar";
-
- while (*str != '\0')
-  _putchar(*str++);
- _putchar('\n');
-
- return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
